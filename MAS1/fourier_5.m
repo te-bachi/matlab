85a0689afb4d1fb2afe8a3cb147fe22bb18e4f17
Andreas Bachmann <bachi@te-clan.ch>
2017-01-05 07:50:23 +0100

    MAS1: modified version of fourier to fourier_5.m

---
+++ MAS1/fourier_5.m
@@ -0,0 +1,48 @@
+
+close all;
+clc;
+clear;
+
+t   = linspace(-pi, pi, 1000);
+
+set(groot, 'defaultAxesTickLabelInterpreter',   'latex');
+set(groot, 'defaultLegendInterpreter',          'latex');
+%set(groot, 'defaultTextInterpreter',            'latex');
+
+% f1
+figure;
+f1  = @(t) sin(2.*t) .* cos(1.*t);
+f2  = @(t) sin(2.*t);
+f3  = @(t) cos(1.*t);
+y1  = f1(t);
+y2  = f2(t);
+y3  = f3(t);
+plot(t, y3, 'Color', [0.4 0.8 0.8], 'LineStyle', '--');
+hold on;
+plot(t, y2, 'Color', [0.8 0.4 0.8], 'LineStyle', '--');
+plot(t, y1, 'Color', 'red', 'LineStyle', '-');
+axis_set('sin \left ( 2 t \right ) \cdot cos \left ( 1  t \right )');
+
+% f2
+f2  = @(t) sin(2.*t) .* cos(2.*t);
+y2  = f2(t);
+figure;
+plot(t, y2, 'Color', 'green', 'LineStyle', '-');
+grid on;
+axis_set('');
+
+% f3
+f3  = @(t) sin(2.*t) .* cos(3.*t);
+y3  = f3(t);
+figure;
+plot(t, y3, 'Color', 'black', 'LineStyle', '--');
+grid on;
+axis_set('');
+
+% f4
+f4  = @(t) sin(2.*t) .* cos(4.*t);
+y4  = f4(t);
+figure;
+plot(t, y4, 'Color', [0.6 0.4 0.2], 'LineStyle', '-');
+grid on;
+axis_set('');
