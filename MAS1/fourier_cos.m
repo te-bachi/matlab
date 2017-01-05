2104f8d5603f369334a51703ea15292f5898ff5e
Andreas Bachmann <bachi@te-clan.ch>
2017-01-05 15:26:58 +0100

    Add cos() and sin() test functions

---
+++ MAS1/fourier_cos.m
@@ -0,0 +1,30 @@
+
+close all;
+clc;
+clear;
+
+
+set(groot, 'defaultAxesTickLabelInterpreter',   'latex');
+set(groot, 'defaultLegendInterpreter',          'latex');
+set(groot, 'defaultTextInterpreter',            'latex');
+
+f1 = 'cos';
+
+figure;
+hold on;
+subplot_m = 2;
+subplot_n = 2;
+                
+t = linspace(-pi, pi, 1000);
+for l = 0:(subplot_m * subplot_n) - 1
+    f  = @(t) cos(l.*t);
+    y  = f(t);
+    subplot(subplot_m, subplot_n, l + 1);
+    plot(t, y);
+    grid on;
+    axis_set_pi(false);
+    legend(['$\mathrm{', f1, '} \left ( ', num2str(l), '  t \right )$']);
+    intpi = sprintf('%4.2f', abs(integral(f, -pi, pi)));
+    fprintf('int(f%d) = %s\n', l, intpi);
+    title(['$\int\limits_{-\pi}^{\pi} \mathrm{', f1, '} \left ( ', num2str(l), ' t \right ) \mathrm{d}t = ', intpi, '$']);
+end
