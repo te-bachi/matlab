d19c4551291935519e92e626412b419f4af27d12
Andreas Bachmann <bachi@te-clan.ch>
2017-04-16 22:16:01 +0200

    PES4: remove old files in directory orientation1

--- PES4/orientation1/myCloseRequestFcn.m
+++
@@ -1,5 +0,0 @@
-function myCloseRequestFcn(src,callbackdata)
-    global CLOSE_FIGURE;
-    CLOSE_FIGURE = 1;
-    delete(gcf);
-end
--- PES4/orientation1/myKeyPressFcn.m
+++
@@ -1,5 +0,0 @@
-function myKeyPressFcn(hObject, event)
-    global KEY_IS_PRESSED
-    KEY_IS_PRESSED  = 1;
-    disp('key pressed!');
-end
